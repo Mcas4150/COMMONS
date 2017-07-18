62fc6538262677dc054410427d671d17076f1997
Michael Cassidy <mikecassidy0000@gmail.com>
2017-07-18 10:47:13 -0400

    front ends

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+  spaces = Space.create([
+    {
+      name:
+      category:
+      capacity:
+      description:
+      price:
+    },
+
+
+
+    ])
