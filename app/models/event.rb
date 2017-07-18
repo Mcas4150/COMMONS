94cab22af3663dd9f09bbc66862723284e7641c1
Michael Cassidy <mikecassidy0000@gmail.com>
2017-07-18 13:19:33 -0400

    adjustments

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,12 +1,12 @@
 class Event < ApplicationRecord
   belongs_to :space
   belongs_to :user
-  has_many :reviewevent
-  validates :name, presence: true
-  validates :admission, presence: true
+  # has_many :reviewevent
+  # validates :name, presence: true
+  # validates :admission, presence: true
   has_attachment :photo
   validates :photo, presence: true
   validates :description, presence: true
-  validates :public, presence: true
+  # validates :public, presence: true
 
 end
