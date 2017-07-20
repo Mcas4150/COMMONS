a0e31f4b555bc75c399ae05a6061819d9d714d61
Michael Cassidy <mikecassidy0000@gmail.com>
2017-07-20 15:19:29 -0400

    add event reviews

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170719160956) do
+ActiveRecord::Schema.define(version: 20170720170802) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 20170719160956) do
     t.date     "from"
     t.date     "to"
     t.integer  "user_id"
+    t.string   "pitch"
     t.index ["space_id"], name: "index_events_on_space_id", using: :btree
     t.index ["user_id"], name: "index_events_on_user_id", using: :btree
   end
