cc1645aeaec6eb082934b6a04a553a6673b4223c
takahiro yokoyama <ytakahiro72@gmail.com>
2017-07-21 11:11:50 -0400

    added past event's photo

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,7 +3,9 @@ class Event < ApplicationRecord
   belongs_to :space
   belongs_to :user
   has_many :reviewevents
-  # validates :name, presence: true
+  has_attachments :images, maximum: 4
+
+  validates :name, presence: true
   # validates :admission, presence: true
 
 
