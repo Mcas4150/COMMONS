19371823a22d2484cdc5bdb4134bcdb12252fdf2
Michael Cassidy <mikecassidy0000@gmail.com>
2017-07-27 15:40:05 -0400

    fixed eventmailer

--- app/mailers/event_mailer.rb
+++ app/mailers/event_mailer.rb
@@ -2,7 +2,7 @@ class EventMailer < ApplicationMailer
   def confirmation(event)
     @event = event
      mail(
-      to:       @event.space.user.email,
+      to:       @event.user.email,
       subject:  "Event #{@event.space.user} created!"
     )
   end
